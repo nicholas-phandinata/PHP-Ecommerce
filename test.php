e99c3f4c41c2a5ac13b9c92803d43aa663cb7c3a
Nicholas Phandinata <nicztheone99@gmail.com>
2022-04-27 19:33:47 +0700

    Upload to github

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>M-Dev Store</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    
+</head>
+<body>
+
+
+
+<script src="js/jquery-331.min.js"></script>
+<script src="js/bootstrap-337.min.js"></script>
+</body>
+</html>
